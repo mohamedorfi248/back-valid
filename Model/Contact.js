279a00a7a62caa626b399c0cf29d4e0da09f942f
meriem <elounimeriem@gmail.com>
2023-07-20 12:23:43 +0100

    first commit

--- Model/Contact.js
+++ Model/Contact.js
@@ -1,22 +1,23 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const schema = mongoose.Schema ;
+const schema = mongoose.Schema;
 
-const contactSchema = new schema ({
-    name : {
-        type : String ,
-        required : true
-    } ,
-    email : {
-        type : String ,
-        required : true ,
-        unique : true
-    } ,
-    phone : Number
-    ,
-  profile_img: String,
-  cloudinary_id: String
-},
-{timestamps : true})
+const contactSchema = new schema(
+	{
+		name: {
+			type: String,
+			required: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		phone: Number,
+		profile_img: String,
+		cloudinary_id: String,
+	},
+	{ timestamps: true }
+);
 
-module.exports = Contact = mongoose.model("contact" , contactSchema)
+module.exports = Contact = mongoose.model("contact", contactSchema);
